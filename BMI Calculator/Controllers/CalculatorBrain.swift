ebab6e15689b2b838f25e2a3f31e9952e0091609
Abdurahman AYDIN <hariysu@gmail.com>
2023-10-05 16:29:38 +0300

    Uses optionals in practice and shows how to use color literals

--- BMI Calculator/Controllers/CalculatorBrain.swift
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -6,17 +6,33 @@
 //  Copyright © 2023 Angela Yu. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct CalculatorBrain {
-    var BMIValue: String?
+    var bmi: BMI?
 
     
     mutating func calculateBMI(height: Float, weight:Float){
-        BMIValue = String(format: "%.1f", weight / pow(height, 2))
+        let bmiValue = weight / pow(height, 2)
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color:.blue)
+        }else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color:.green)
+        }else {
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color:.red)
+        }
     }
     
     func getBMIValue() ->String {
-        return BMIValue!
+        let bmiTo1DecimalPlace = String(format: "%.1f", bmi?.value ?? 0.0)
+        return bmiTo1DecimalPlace        
+    }
+    
+    func getAdvice() -> String {
+        return bmi?.advice ?? ""
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? .white
     }
 }
